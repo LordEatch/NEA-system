e25204e1ef500f6940252178fb42d2fe5f667fc2
Home <chrisgodwin67@gmail.com>
2022-10-03 22:53:02 +0100

    Added 'Logout' menu item.

--- NEA system/AppShell.xaml.cs
+++ NEA system/AppShell.xaml.cs
@@ -11,4 +11,10 @@ public partial class AppShell : Shell
 		Routing.RegisterRoute(nameof(Page_CreateUser), typeof(Page_CreateUser));
         Routing.RegisterRoute(nameof(Page_EnterPassword), typeof(Page_EnterPassword));
     }
+
+	//FINISH does this break MVVM by having navigation in a view?
+	private void LogoutClicked(object sender, EventArgs e)
+	{
+		Shell.Current.GoToAsync($"//{nameof(Page_Login)}");
+	}
 }
