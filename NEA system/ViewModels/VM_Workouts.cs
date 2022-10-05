13c11f7f25ca470994027f864046d91768828967
LordEatch <113691768+LordEatch@users.noreply.github.com>
2022-10-05 10:44:43 +0100

    Update VM_Workouts.cs
    Changed RefreshWorkouts() and added a property called Search and a method called FilterWorkouts(). Need to fill skeleton code. Now, every time a user updates Search, the OC will refresh with the array returned by FilterWorkouts(Search).

--- NEA system/ViewModels/VM_Workouts.cs
+++ NEA system/ViewModels/VM_Workouts.cs
@@ -8,15 +8,26 @@ internal class VM_Workouts : VM_DbAccessor
     // Properties
 
     public User MyUser { get; set; }
-
-
-    //  filters and sort_by here
-
-    public string NumberOfWorkouts { get; set; }
+    
+    public string Search
+    {
+        get;
+        set
+        {
+            //Search = value;
+		    //if (Seach not changed in 1 second)
+		    //{
+		    //	RefreshWorkouts(Search);
+		    //}
+        }
+    }
+    
     public ObservableCollection<Workout> Workouts { get; set; }
+    public string NumberOfWorkouts { get; set; }
 
 
     public Command WorkoutSelectedCommand { get; }
+    public Command GoToPage_CreateWorkout { get; }
     //test
     public Command TestCommand { get; }
 
@@ -27,6 +38,7 @@ internal class VM_Workouts : VM_DbAccessor
     public VM_Workouts()
     {
         WorkoutSelectedCommand = new Command<Workout>(WorkoutSelected);
+        //GoToPage_CreateWorkout = new Command(()=> Shell.Current.GoToAsync(nameof(Page_CreateWorkout)));
 
         Workouts = new ObservableCollection<Workout>();
 
@@ -41,23 +53,16 @@ internal class VM_Workouts : VM_DbAccessor
     public void InitialiseVM()
     {
         RefreshWorkouts();
+        
         //I would have this as a getter under the property but MAUI bug means that query props
         //do not pass until some time after the page appears (need UserID to get number of workouts).
-        //This method is delayed later to fix.
-
+        //This method is delayed in Page.OnAppearing() to fix.
         NumberOfWorkouts = db.Table<Workout>().Where(w => w.UserID == MyUser.UserID).Count().ToString();
         OnPropertyChanged(nameof(NumberOfWorkouts));
     }
-
-    private void RefreshWorkouts()
-    {
-        Workouts.Clear();
-        foreach (var workout in db.Table<Workout>().Where(w => w.UserID == MyUser.UserID).ToArray())
-        {
-            Workouts.Add(workout);
-        }
-    }
-
+    
+    
+    
     //test
     private void Test()
     {
@@ -70,6 +75,43 @@ internal class VM_Workouts : VM_DbAccessor
         db.Insert(workout);
 
         RefreshWorkouts();
+    }
+    
+    
+    
+    
+
+    private void RefreshWorkouts(string filter)
+    {
+        Workouts.Clear();
+        //foreach (workout in FilterWorkouts(filter))
+		//{
+		//  Workouts.Add(workout);
+		//}
+
+
+        
+        //OLD CODE
+        //foreach (var workout in db.Table<Workout>().Where(w => w.UserID == MyUser.UserID).ToArray())
+        //{
+        //    Workouts.Add(workout);
+        //}
+    }
+
+    private Workout[] FilterWorkouts(string filter)
+    {
+        //if (filter != NullOrWhiteSpace)
+	    //{
+		//    Return anything relevant in the database that is equal to the filter:
+
+		//    Workout properties,
+		//    Exercise properties (return the workout associated with those sets)
+		//    Set properties (return the workout associated with those sets)
+	    //}
+        //else
+        //{
+        //  return all workouts;
+        //}
     }
 
     private void WorkoutSelected(Workout workout)
