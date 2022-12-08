8f750099b685d7b1fd882abd64a17b0615855fb8
x21cgodwin <christopherg16@stu.ben.srscmat.co.uk>
2022-12-08 14:59:38 +0000

    Added property to Exercise class.

--- NEA system/Models/Exercise.cs
+++ NEA system/Models/Exercise.cs
@@ -2,23 +2,35 @@
 
 namespace NEA_system.Models;
 
-    public class Exercise
+public class Exercise
+{
+    //Properties
+
+    [PrimaryKey, AutoIncrement]
+    public int ExerciseID { get; set; }
+    [NotNull]
+    public int WorkoutID { get; set; }
+    [NotNull]
+    public int ExerciseTypeID { get; set; }
+    //FINISH Not sure if this works??
+    [Ignore]
+    public string ExerciseName
     {
-        //Properties
-
-        [PrimaryKey, AutoIncrement]
-        public int ExerciseID { get; set; }
-        [NotNull]
-        public int WorkoutID { get; set; }
-        [NotNull]
-        public int ExerciseTypeID { get; set; }
-
-
-
-        //Methods
-
-        public static Exercise[] PullExercises(SQLiteConnection db, int workoutID)
+        get
         {
-            return db.Table<Exercise>().Where(exercise => exercise.WorkoutID.Equals(workoutID)).ToArray();
+            var exerciseTypes = Session.DB.Table<ExerciseType>().Where(eT => eT.ExerciseTypeID == ExerciseTypeID).ToArray();
+            if (exerciseTypes == null)
+            {
+                return "No corresponding exercise type found.";
+            }
+            else
+            {
+                return exerciseTypes[0].ExerciseTypeName;
+            }
         }
+    }
+
+    public static IEnumerable<string> QueryVals()
+    {
+        return Session.DB.Query<string>("SELECT \"ExerciseTypeName\" FROM ExerciseType WHERE ExerciseID = ?", this.ExerciseID);
     }
