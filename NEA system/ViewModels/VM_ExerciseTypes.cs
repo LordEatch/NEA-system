2da29e3b90cc2a3891c8a4d92b5230c9611fc8b7
Home <chrisgodwin67@gmail.com>
2023-02-05 20:16:42 +0000

    Added UserID parameter to SQL query on Page_ExerciseTypes.

--- NEA system/ViewModels/VM_ExerciseTypes.cs
+++ NEA system/ViewModels/VM_ExerciseTypes.cs
@@ -32,7 +32,16 @@ internal class VM_ExerciseTypes : VM_Base, IDatabaseOutput
     public void LoadViewData()
     {
         ExerciseTypes.Clear();
-        foreach (ExerciseType eT in Session.DB.Table<ExerciseType>())
+
+        //Query returns exercise types linked to a user via a record in the subscription table.
+        string query = @$"
+                SELECT DISTINCT ExerciseType.ExerciseTypeID, ExerciseType.ExerciseTypeName, ExerciseType.ExerciseTypeDescription
+                FROM ExerciseType
+                INNER JOIN Subscription ON Subscription.ExerciseTypeID = ExerciseType.ExerciseTypeID
+                WHERE Subscription.UserID = '{Session.CurrentUser.UserID}'";
+
+        //Populate the observable collection.
+        foreach (ExerciseType eT in Session.DB.Query<ExerciseType>(query))
         {
             ExerciseTypes.Add(eT);
         }
